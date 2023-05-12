bdf86b0563402e997051878477d429d8e12b26b2
Geovanna Otoni <geovannaotoni@gmail.com>
2023-05-12 15:51:11 -0300

    requisito 3

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -1,11 +1,115 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
+  state = {
+    valueInput: 0,
+    description: '',
+    currency: 'USD',
+    method: 'dinheiro',
+    tag: 'alimentacao',
+  };
+
+  // logo que o componente é montado, realiza a requisição para a API para obter a lista de moedas com a getCurrencies()
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(getCurrencies());
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { valueInput, description, currency, method, tag } = this.state;
+    const { currencies } = this.props;
+    // console.log(currencies);
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="valueInput">
+          Valor:
+          <input
+            type="number"
+            name="valueInput"
+            value={ valueInput }
+            onChange={ this.handleChange }
+            id="valueInput"
+            data-testid="value-input"
+          />
+        </label>
+        <label htmlFor="description">
+          Descrição:
+          <input
+            type="text"
+            name="description"
+            value={ description }
+            onChange={ this.handleChange }
+            id="description"
+            data-testid="description-input"
+          />
+        </label>
+        <label htmlFor="currency">
+          Moeda:
+          <select
+            name="currency"
+            id="currency"
+            data-testid="currency-input"
+            value={ currency }
+            onChange={ this.handleChange }
+          >
+            {currencies.map((coin) => (
+              <option value={ coin } key={ coin }>
+                {coin}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="method">
+          Método de Pagamento:
+          <select
+            name="method"
+            id="method"
+            data-testid="method-input"
+            value={ method }
+            onChange={ this.handleChange }
+          >
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Cartão de crédito</option>
+            <option value="debito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Categoria:
+          <select
+            name="tag"
+            id="tag"
+            data-testid="tag-input"
+            value={ tag }
+            onChange={ this.handleChange }
+          >
+            <option value="alimentacao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saude">Saúde</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
 
-export default WalletForm;
+WalletForm.propTypes = {
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -15,3 +15,20 @@ export const requestSuccessful = (data) => ({
   type: REQUEST_SUCCESSFUL,
   payload: data,
 });
+
+// thunk action creator: deve retornar uma função
+export function getCurrencies() {
+  return async (dispatch) => {
+    try {
+      // realiza o fetch para a API
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      // o data é o objeto de objetos
+      const data = await response.json();
+      // necessário transformar o data em um array de strings, por isso, utiliza-se o Object.keys para selecionar somente as chaves de cada objeto (que são as moedas) e em seguida usar o filter para remover a moeda USDT
+      const arrayOfData = Object.keys(data).filter((currency) => currency !== 'USDT');
+      dispatch(requestSuccessful(arrayOfData));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
