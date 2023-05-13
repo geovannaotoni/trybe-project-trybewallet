7501adc025be4b845ae5d8850f2423b482ef89f7
Geovanna Otoni <geovannaotoni@gmail.com>
2023-05-13 16:41:10 -0300

    inicio requisito 10

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,13 +1,17 @@
-import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 // import Wallet from '../pages/Wallet';
 import mockData from './helpers/mockData';
 import App from '../App';
 
+const valueInputTestId = 'value-input';
+const totalFieldTestId = 'total-field';
+
 describe('Testes para a página Wallet', () => {
   const initialEntries = ['/carteira'];
   const expenseExample1 = {
+    id: 0,
     value: '10',
     currency: 'USD',
     method: 'Dinheiro',
@@ -16,6 +20,7 @@ describe('Testes para a página Wallet', () => {
     exchangeRates: mockData,
   };
   const expenseExample2 = {
+    id: 1,
     value: '1',
     currency: 'USD',
     method: 'Cartão de crédito',
@@ -41,7 +46,7 @@ describe('Testes para a página Wallet', () => {
     expect(screen.getByText(/teste@teste\.com/i)).toBeInTheDocument();
     expect(screen.getByText(/brl/i)).toBeInTheDocument();
     expect(screen.getByText(/0\.00/i)).toBeInTheDocument();
-    expect(screen.getByTestId('value-input')).toBeInTheDocument();
+    expect(screen.getByTestId(valueInputTestId)).toBeInTheDocument();
     expect(screen.getByTestId('description-input')).toBeInTheDocument();
     expect(screen.getByTestId('currency-input')).toBeInTheDocument();
     expect(screen.getByTestId('method-input')).toBeInTheDocument();
@@ -60,6 +65,58 @@ describe('Testes para a página Wallet', () => {
       },
     };
     renderWithRouterAndRedux(<App />, { initialState, initialEntries });
-    expect(screen.getByTestId('total-field')).toHaveTextContent('52.28');
+    expect(screen.getByTestId(totalFieldTestId)).toHaveTextContent('52.28');
+  });
+
+  it('Verifica o botão de editar despesa', async () => {
+    const initialState = {
+      wallet: {
+        expenses: [expenseExample1, expenseExample2],
+        currencies: Object.keys(mockData).filter((currency) => currency !== 'USDT'),
+        editor: false,
+        idToEdit: 0,
+      },
+    };
+    renderWithRouterAndRedux(<App />, { initialState, initialEntries });
+
+    const btnEditar = screen.getAllByRole('button', { name: /editar/i });
+    userEvent.click(btnEditar[0]);
+
+    const valueInput = screen.getByTestId(valueInputTestId);
+    userEvent.type(valueInput, '100');
+    // expect(valueInput).toHaveValue(100);
+    const btnEnviarEdicao = screen.getByRole('button', { name: /editar despesa/i });
+    userEvent.click(btnEnviarEdicao);
+    await waitForElementToBeRemoved(() => screen.getByText('52.28'));
+    expect(screen.getByTestId(totalFieldTestId)).toHaveTextContent('480.06');
+  });
+
+  it('Verifica o botão de deletar despesa', () => {
+    const initialState = {
+      wallet: {
+        expenses: [expenseExample1, expenseExample2],
+        currencies: Object.keys(mockData).filter((currency) => currency !== 'USDT'),
+      },
+    };
+    renderWithRouterAndRedux(<App />, { initialState, initialEntries });
+
+    const btnExcluir = screen.getAllByRole('button', {
+      name: /excluir/i,
+    });
+    console.log(btnExcluir);
+    userEvent.click(btnExcluir[0]);
+    expect(screen.getByTestId(totalFieldTestId)).toHaveTextContent('4.75');
+  });
+
+  it('Verifica se ao adicionar uma despesa, os inputs são limpos', () => {
+    // renderWithRouterAndRedux(<App />, { initialState, initialEntries });
+    // const valueInput = screen.getByTestId(valueInputTestId);
+    // const descriptionInput = screen.getByTestId('description-input');
+    // const currencyInput = screen.getByTestId('currency-input');
+    // const methodInput = screen.getByTestId('method-input');
+    // const tagInput = screen.getByTestId('tag-input');
+    // const btnAddExpense = screen.getByRole('button', {
+    //   name: /adicionar despesa/i,
+    // });
   });
 });
